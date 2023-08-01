8cd87e5c7ef77d0df568eab91b8eba9dc8b6fa5f
James Chiedozie James <110996482+chiedozie013@users.noreply.github.com>
2023-08-01 19:14:29 +0100

    linters check

--- script.js
+++ script.js
@@ -1,79 +1,77 @@
-const btn = document.querySelector(".hamburger");
-const showHamburger = document.querySelector(".showHamburger");
-const menuLinks = document.querySelectorAll(".menu-links");
-const closeBtn = document.querySelector(".close-cross");
+const btn = document.querySelector('.hamburger');
+const showHamburger = document.querySelector('.showHamburger');
+const menuLinks = document.querySelectorAll('.menu-links');
+const closeBtn = document.querySelector('.close-cross');
 // Speakers
-const speakers = document.querySelector(".speakers");
-const more = document.querySelector(".more-btn");
-const less = document.querySelector(".less-btn");
+const speakers = document.querySelector('.speakers');
+const more = document.querySelector('.more-btn');
+const less = document.querySelector('.less-btn');
 
 // Mobile Menu
 function openButton() {
-  showHamburger.classList.add("show-showHamburger");
+  showHamburger.classList.add('show-showHamburger');
 }
 
-btn.addEventListener("click", openButton);
+btn.addEventListener('click', openButton);
 
 function closeButton() {
-  showHamburger.classList.remove("show-showHamburger");
+  showHamburger.classList.remove('show-showHamburger');
 }
 
-closeBtn.addEventListener("click", closeButton);
+closeBtn.addEventListener('click', closeButton);
 
-menuLinks.forEach((link) =>
-  link.addEventListener("click", () => {
-    showHamburger.classList.remove("show-showHamburger");
-  })
-);
+menuLinks.forEach((link) => link.addEventListener('click', () => {
+  showHamburger.classList.remove('show-showHamburger');
+}));
 
 const mainSpeakers = [
   {
     id: 1,
-    img: "./assets/images/speaker-1.png",
-    name: "Yochai Benkler",
-    specialization: "Spanning Artificial Intelligence",
+    img: './assets/images/speaker-1.png',
+    name: 'Yochai Benkler',
+    specialization: 'Spanning Artificial Intelligence',
     description:
-      "Benkler is a spanning artificial intelligence specialist with over 23years of experience in the UK.",
+      'Benkler is a spanning artificial intelligence specialist with over 23years of experience in the UK.',
   },
   {
     id: 2,
-    img: "./assets/images/speaker-2.png",
-    name: "Ayo Ogunbade",
-    specialization: "Blockchain specialist and CEO dubigs",
+    img: './assets/images/speaker-2.png',
+    name: 'Ayo Ogunbade',
+    specialization: 'Blockchain specialist and CEO dubigs',
     description:
-      "Ogunbade is a blockchain specialist with over 10years of experience in the Africa.",
+      'Ogunbade is a blockchain specialist with over 10years of experience in the Africa.',
   },
   {
     id: 3,
-    img: "./assets/images/speaker-3.png",
-    name: "Anita Bricks",
-    specialization: "Data Scientist and MD Swiss Bank",
+    img: './assets/images/speaker-3.png',
+    name: 'Anita Bricks',
+    specialization: 'Data Scientist and MD Swiss Bank',
     description:
-      "Bricks is a data scientist with over 13years of experience in the bank spector.",
+      'Bricks is a data scientist with over 13years of experience in the bank spector.',
   },
   {
     id: 4,
-    img: "./assets/images/speaker-4.png",
-    name: "Fedalis Ayuw",
-    specialization: "Artificial Intelligence",
+    img: './assets/images/speaker-4.png',
+    name: 'Fedalis Ayuw',
+    specialization: 'Artificial Intelligence',
     description:
-      "Ayuw is a artificial intelligence specialist with over 15years of experience in Ghana and CEO of DGT Africa.",
+      'Ayuw is a artificial intelligence specialist with over 15years of experience in Ghana and CEO of DGT Africa.',
   },
   {
     id: 5,
-    img: "./assets/images/speaker-5.png",
-    name: "Chiwendu Bankole",
-    specialization: "Product Designer",
+    img: './assets/images/speaker-5.png',
+    name: 'Chiwendu Bankole',
+    specialization: 'Product Designer',
     description:
       "Bankole is a product designer with over 16years of experience in the UK and Nigeria. She's the pioneer of TT Nigeria",
   },
   {
     id: 6,
-    img: "./assets/images/speaker-6.png",
-    name: "Yusef Rahman",
-    specialization: "Robotics",
+    img: './assets/images/speaker-6.png',
+    name: 'Yusef Rahman',
+    specialization: 'Robotics',
     description:
-      "Rahman is a robotics specialist with over 20years of experience in the US and beyond.",
+      'Rahman is a robotics specialist with over 20years of experience in the US and beyond.',
   },
 ];
 
@@ -102,38 +100,38 @@ const speaker = function () {
   }
 };
 
-window.addEventListener("load", speaker);
+window.addEventListener('load', speaker);
 
 // More button
 let currentItems = 2;
 
-more.addEventListener("click", () => {
-  const moreSpeakers = [...document.querySelectorAll(".speaker-details")];
+more.addEventListener('click', () => {
+  const moreSpeakers = [...document.querySelectorAll('.speaker-details')];
 
   for (let index = currentItems; index < moreSpeakers.length; index += 1) {
-    moreSpeakers[index].style.display = "flex";
+    moreSpeakers[index].style.display = 'flex';
   }
 
   currentItems += 4;
 
   if (currentItems >= moreSpeakers.length) {
-    more.style.display = "none";
-    less.classList.remove("hidden");
+    more.style.display = 'none';
+    less.classList.remove('hidden');
   }
 });
 
 // Less button
-less.addEventListener("click", () => {
-  const moreSpeakers = [...document.querySelectorAll(".speaker-details")];
+less.addEventListener('click', () => {
+  const moreSpeakers = [...document.querySelectorAll('.speaker-details')];
 
   for (let index = 2; index < moreSpeakers.length; index += 1) {
-    moreSpeakers[index].style.display = "none";
+    moreSpeakers[index].style.display = 'none';
   }
 
   currentItems -= 4;
 
   if (currentItems <= 2) {
-    more.style.display = "flex";
-    less.classList.add("hidden");
+    more.style.display = 'flex';
+    less.classList.add('hidden');
   }
 });
